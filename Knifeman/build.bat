adeebf9df6425017f8af0a7162d236f35b2f7a43
spacebruce <noel.abercrombie@hotmail.com>
2022-10-11 12:32:39 +0100

    Auto create build directory

--- Knifeman/build.bat
+++ Knifeman/build.bat
@@ -1,3 +1,4 @@
+mkdir build
 cd src
 dasm knifeman.asm -f3 -v0 -sKNIFEMAN_PAL.sym -lKNIFEMAN_PAL.lst -DREGION=0 -oKNIFEMAN_PAL.a26
 dasm knifeman.asm -f3 -v0 -sKNIFEMAN_NTSC.sym -lKNIFEMAN_NTSC.lst -DREGION=1 -oKNIFEMAN_NTSC.a26 
